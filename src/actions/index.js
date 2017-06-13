e8185a32cee0d157429f2138d0b3eff56e9952e3
jonathanmaia <jonathanmaia@trt7.jus.br>
2017-06-13 15:19:33 -0300

    Polishing code

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '9fe7c6a086743a798b30f919f64e5821';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
